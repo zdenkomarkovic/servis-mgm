590db295f28966ee7ec7d5f1743021b15a9c1041
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-08-22 10:13:59 +0200

    .

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://servisracunaramgm.rs", // Zameni svojim domenom
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+};
